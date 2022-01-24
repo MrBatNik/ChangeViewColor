82d6c1885311485b9559fbf134567e18d8fabab4
Nikita <nicandreo@gmail.com>
2022-01-24 15:15:24 +0300

    Initial Commit

---
+++ ChangeViewColor/ChangeViewColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeViewColorApp.swift
+//  ChangeViewColor
+//
+//  Created by Nikita Batrakov on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeViewColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
